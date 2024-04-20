59a48c942b772b711e503964c21bafc559403a8d
bbkmg256 <tecded@proton.me>
2024-04-20 12:05:40 -0300

    Se implementó la ejecucion de mods, pero desde un dir elegido

--- modules/ctrl.py
+++ modules/ctrl.py
@@ -14,11 +14,12 @@ config_file="./config.INI" # Ruta de fichero de configuración
 
 
 # Inicia el engiene con los parametros configurados.
-def exe_all(engine, iwad):
+def exe_all(engine, iwad, pwads):
 	print(f"\n(!) Ejecutando Doom ...")
-	print(f"[Info] \nEngine: {engine.value} \nIWAD: {iwad.value}\n")
+	print(f"[Info] \nEngine: {engine.value} \nIWAD: {iwad.value} \nMods dir.: {pwads.value}\n")
 
-	if engine.value != iwad.value and iwad.value != "":
+	#if engine.value != iwad.value and iwad.value != "":
+	if engine.value != iwad.value and iwad.value != pwads.value and pwads.value != "":
 		'''
 		# Parse pwads to list
 		i = 0
@@ -31,10 +32,10 @@ def exe_all(engine, iwad):
 		'''
 		config['CFG']['engine_path'] = engine.value
 		config['CFG']['iwad_path'] = iwad.value
-		#config['CFG']['pwads'] = cad_pwads
+		config['CFG']['pwads'] = pwads.value
 		config.write(open(config_file, 'w'))
 		# Execute engine with iwad
-		command = [engine.value, "-iwad", iwad.value, "-file"]
+		command = [engine.value, "-iwad", iwad.value, "-file", f"{pwads.value}/*"]
 		'''
 		if bool(lista_pwads):
 			for i in lista_pwads:
@@ -46,15 +47,24 @@ def exe_all(engine, iwad):
 		print(f"Ingrese valores validos para los campos de valores xd")
 
 # Obtiene las rutas mediante un FileDialog.
-def get_path(e: fl.FilePickerResultEvent, val_tf):
-	if e.files:
-		print(f"Fichero '{e.files[0].name}' seleccionado: {e.files[0].path}")
-		val_tf.value = e.files[0].path
-		val_tf.update()
-	else: print(f"(!) No se seleccionó ningun fichero!\n")
+def get_path(e: fl.FilePickerResultEvent, val_tf, directory):
+	if directory:
+		# Directorio
+		if e.path:
+			print(f"(!) Seleccionado dir.: {e.path}")
+			val_tf.value = e.path
+			val_tf.update()
+		else: print(f"(!) No se seleccionó ningun directorio!\n")
+	else:
+		# Fichero
+		if e.files:
+			print(f"(!) Seleccionado fichero: {e.files[0].path}")
+			val_tf.value = e.files[0].path
+			val_tf.update()
+		else: print(f"(!) No se seleccionó ningun fichero!\n")
 
 # Verifica la existencia de un fichero de configuracion y en caso favorable, lo lee.
-def verify_config(eng_tf, iwad_tf):	
+def verify_config(eng_tf, iwad_tf, pwads_tf):	
 	if not (os.path.exists(config_file)):
 		print(f"(!) Fichero de configuración no encontrado, se ah creado uno...")
 		config['CFG'] = {
@@ -68,3 +78,9 @@ def verify_config(eng_tf, iwad_tf):
 		config.read(config_file)
 		eng_tf.value = config['CFG']['engine_path']
 		iwad_tf.value = config['CFG']['iwad_path']
+		pwads_tf.value = config['CFG']['pwads']
+
+def clean(engine, iwad, pwads):
+	engine.value, iwad.value, pwads.value = "", "", ""
+	engine.update(); iwad.update(); pwads.update()
+	print(f"(!) Campos limpiados!\n")
--- modules/ui.py
+++ modules/ui.py
@@ -10,27 +10,34 @@ from . import ctrl
 def main_ui(page: fl.Page):
     # Definición de valores y catacteristicas
     page.title = "Dumex"
-    page.window_width, page.window_height = 1000, 500
+    page.window_width, page.window_height = 500, 300
     #page.window_resizable = False # No funciona bien en linux
     page.update()
 
     engine_tf = fl.TextField(label="Ruta del Engine (GZDOOM)")
     iwad_tf = fl.TextField(label="Ruta del .IWAD")
-    # pwad = ...
+    pwad_tf = fl.TextField(label="Ruta dir. Mods")
 
     # Verifica configuracion
-    ctrl.verify_config(eng_tf=engine_tf, iwad_tf=iwad_tf)
+    ctrl.verify_config(eng_tf=engine_tf, iwad_tf=iwad_tf, pwads_tf=pwad_tf)
     
     # FileDialog para Engine
-    picker_fd_engine = fl.FilePicker(on_result=lambda e: ctrl.get_path(e, val_tf=engine_tf)); page.overlay.append(picker_fd_engine)
+    picker_fd_engine = fl.FilePicker(on_result=lambda e: ctrl.get_path(e, val_tf=engine_tf, directory=False)); page.overlay.append(picker_fd_engine)
     bt_picker_fd_engine = fl.ElevatedButton(text="Buscar Engine", on_click=lambda _: picker_fd_engine.pick_files(allow_multiple=False))
    
     # FileDialog para IWAD
-    picker_fd_iwad = fl.FilePicker(on_result=lambda e: ctrl.get_path(e, val_tf=iwad_tf)); page.overlay.append(picker_fd_iwad)
+    picker_fd_iwad = fl.FilePicker(on_result=lambda e: ctrl.get_path(e, val_tf=iwad_tf, directory=False)); page.overlay.append(picker_fd_iwad)
     bt_picker_fd_iwad = fl.ElevatedButton(text="Buscar IWAD", on_click=lambda _: picker_fd_iwad.pick_files(allow_multiple=False))
 
+    # FileDialog para PWADs
+    picker_fd_pwad = fl.FilePicker(on_result=lambda e: ctrl.get_path(e, val_tf=pwad_tf, directory=True)); page.overlay.append(picker_fd_pwad)
+    bt_picker_fd_pwad = fl.ElevatedButton(text="Buscar dir. Mods", on_click=lambda _: picker_fd_pwad.get_directory_path())
+
     # Boton Iniciar xd
-    bt_start = fl.ElevatedButton(text="INICIAR", on_click=lambda _: ctrl.exe_all(engine=engine_tf, iwad=iwad_tf))
+    bt_start = fl.ElevatedButton(text="INICIAR", on_click=lambda _: ctrl.exe_all(engine=engine_tf, iwad=iwad_tf, pwads=pwad_tf))
+
+    # Boton Limpiar todo xd
+    bt_clean = fl.ElevatedButton(text="Limpiar", on_click=lambda _: ctrl.clean(engine=engine_tf, iwad=iwad_tf, pwads=pwad_tf))
 
     # Añadiendo los widgets a la ventana
     page.add(
@@ -43,6 +50,12 @@ def main_ui(page: fl.Page):
             fl.Row([
                 iwad_tf, bt_picker_fd_iwad
             ]),
-            bt_start
+            fl.Row([
+                pwad_tf, bt_picker_fd_pwad
+            ]),
+            fl.Row([
+                bt_start,
+                bt_clean
+            ])
         ])
     )
