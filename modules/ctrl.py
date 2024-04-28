9bf5088a8da91388d9065d2f152ca09e3adcef69
bbkmg256 <bbkmg256@proton.me>
2024-04-28 14:30:13 -0300

    mejorado aspectos del codigo

--- modules/ctrl.py
+++ modules/ctrl.py
@@ -12,21 +12,94 @@ import subprocess as sp
 user_name = sp.run(["whoami"], capture_output=True, text=True)
 # Instanciación de configparser
 config = cp.ConfigParser(); cf_name = 'dumex_conf.INI'
-
+global cf_path
 
 # Definición de ruta de fichero
-def os_reco():
-	global cf_path
-	if os.name == "nt":
-		cf_path = f'C:\\Documents\\dumex\\{cf_name}'.replace ("\n", "") # Win
-		print(f"(!) Entorno encontrado: Windows\n")
-	elif os.name == "posix":
-		cf_path = f'/home/{user_name.stdout}/.config/dumex/{cf_name}'.replace ("\n", "") # Unix
-		print(f"(!) Entorno encontrado: Unix\n")
+if os.name == "nt":
+	cf_path = f'C:\\Documents\\dumex\\{cf_name}'.replace ("\n", "") # Win
+	print(f"(!) Entorno encontrado: Windows\n")
+elif os.name == "posix":
+	cf_path = f'/home/{user_name.stdout}/.config/dumex/{cf_name}'.replace ("\n", "") # Unix
+	print(f"(!) Entorno encontrado: Unix\n")
+else:
+	# Forma de manejar el fichero en S.O raros xd
+	cf_path = f"./{cf_name}"
+	print(f"(!) OS no identificado!\n")
+
+# Comprueba existencia de directorio
+if not (os.path.exists(cf_path[:len(cf_path) - (len(cf_name)+1)])):
+	print(f"(!) No se encontro directorio de configuración, se ah creado uno...")
+	# Crea dir.
+	sp.run(["mkdir", f"{cf_path[:len(cf_path) - (len(cf_name)+1)]}"])
+	# comprueba existencia de fichero
+if not (os.path.exists(cf_path)):
+	print(f"(!) Fichero de configuración no encontrado, se ah creado uno...")
+	# Escribe fichero
+	config['CFG'] = {
+		'engine_path' : '',
+		'iwad_path' : '',
+		'pwads': '',
+		'mods_enable': 'False'
+		}
+	config.write(open(cf_path, 'w'))
+else: print(f"(!) Fichero de configuración encontrado...")
+
+# Seteo de engine, iwad y pwads
+def set_engine_cfg() -> str():
+	#print(f"Funcionando")
+	config.read(cf_path) # Lee fichero
+	return config['CFG']['engine_path']
+
+def set_iwad_cfg() -> str():
+	config.read(cf_path)
+	return config['CFG']['iwad_path']
+
+def set_pwads_cfg() -> str():
+	config.read(cf_path)
+	return config['CFG']['pwads']
+
+# Seteo (habilitación) del tf, bt y cb correspondientes a pwads
+def set_tf_pwads_cfg() -> bool:
+	config.read(cf_path)
+	return not eval(config['CFG']['mods_enable'])
+
+def set_bt_pwads_cfg() -> bool:
+	config.read(cf_path)
+	return not eval(config['CFG']['mods_enable'])
+
+def set_cb_pwads_cfg() -> bool:
+	config.read(cf_path)
+	return eval(config['CFG']['mods_enable'])
+
+# Obtiene las rutas mediante un FileDialog.
+def get_path(e: fl.FilePickerResultEvent, val_tf, directory):
+	if directory:
+		# Directorio
+		if e.path:
+			print(f"(!) Seleccionado dir.: {e.path}")
+			val_tf.value = e.path
+			val_tf.update()
+		else: print(f"(!) No se seleccionó ningun directorio!\n")
 	else:
-		# Forma de manejar el fichero en S.O raros xd
-		cf_path = f"./{cf_name}"
-		print(f"(!) OS no identificado!\n")
+		# Fichero
+		if e.files:
+			print(f"(!) Seleccionado fichero: {e.files[0].path}")
+			val_tf.value = e.files[0].path
+			val_tf.update()
+		else: print(f"(!) No se seleccionó ningun fichero!\n")
+
+# Limpia los campos de texto
+def clean(*args):
+	for comp in args:
+		comp.value = ""
+	print(f"(!) Campos limpiados!\n")
+
+# Deshabilita componente
+def disable_comp(*args):
+	print(f"(!) Mods: {args[0].disabled}\n")
+	for comp in args:
+		comp.disabled = not comp.disabled
+		comp.update()
 
 # Inicia el engine con los parametros configurados.
 def exe_all(engine, iwad, pwads, pwads_cb):
@@ -46,14 +119,15 @@ def exe_all(engine, iwad, pwads, pwads_cb):
 	# Save config
 	cad_pwads = ", ".join(lista_pwads)
 	'''
+	# lista para el comando a ejecutar
+	command = [engine.value, "-iwad", iwad.value]
 	# Si, ya sé que esto no está bien, es momentaneo
 	if not pwads.disabled:
 		if engine.value != iwad.value != pwads.value != "":
 			print(f"\n(!) Ejecutando Doom con mods ...")
 			print(f"[Info] \nEngine: {engine.value} \nIWAD: {iwad.value} \nMods dir.: {pwads.value}\n")
-			
 			# lista para el comando a ejecutar
-			command = [engine.value, "-iwad", iwad.value, "-file", f"{pwads.value}/*"]
+			for i in ["-file", f"{pwads.value}/*"]: command.append(i)
 			'''
 			if bool(lista_pwads):
 				for i in lista_pwads:
@@ -61,81 +135,10 @@ def exe_all(engine, iwad, pwads, pwads_cb):
 				print(command)
 			'''
 			sp.run(command)
-		else:
-			print(f"(!) Ingrese valores validos para los campos de valores xd")
+		else: print(f"(!) Ingrese valores validos para los campos de valores xd")
 	else:
 		if engine.value != iwad.value != "":
 			print(f"\n(!) Ejecutando Doom sin mods ...")
 			print(f"[Info] \nEngine: {engine.value} \nIWAD: {iwad.value}\n")
-			
-			config['CFG']['engine_path'], config['CFG']['iwad_path'] = engine.value, iwad.value
-			config.write(open(cf_path, 'w'))
-			# lista para el comando a ejecutar
-			command = [engine.value, "-iwad", iwad.value]
 			sp.run(command)
-		else:
-			print(f"(!) Ingrese valores validos para los campos de valores xd")
-
-# Obtiene las rutas mediante un FileDialog.
-def get_path(e: fl.FilePickerResultEvent, val_tf, directory):
-	if directory:
-		# Directorio
-		if e.path:
-			print(f"(!) Seleccionado dir.: {e.path}")
-			val_tf.value = e.path
-			val_tf.update()
-		else: print(f"(!) No se seleccionó ningun directorio!\n")
-	else:
-		# Fichero
-		if e.files:
-			print(f"(!) Seleccionado fichero: {e.files[0].path}")
-			val_tf.value = e.files[0].path
-			val_tf.update()
-		else: print(f"(!) No se seleccionó ningun fichero!\n")
-
-# Verifica la la configuración.
-def verify_config(eng_tf, iwad_tf, pwads_tf, pwads_bt, pwads_cb):
-	os_reco() # Reconocimiento de S.O
-	# Comprueba existencia de directorio
-	if not (os.path.exists(cf_path[:len(cf_path) - (len(cf_name)+1)])):
-		print(f"(!) No se encontro directorio de configuración, se ah creado uno...")
-		# Crea dir.
-		sp.run(["mkdir", f"{cf_path[:len(cf_path) - (len(cf_name)+1)]}"])
-	# comprueba existencia de fichero
-	if not (os.path.exists(cf_path)):
-		print(f"(!) Fichero de configuración no encontrado, se ah creado uno...")
-		# Escribe fichero
-		config['CFG'] = {
-			'engine_path' : '',
-			'iwad_path' : '',
-			'pwads': '',
-			'mods_enable': 'False'
-			}
-		config.write(open(cf_path, 'w'))
-	else:
-		print(f"(!) Fichero de configuración encontrado...")
-		# Lee fichero
-		config.read(cf_path)
-		# Campo texto
-		eng_tf.value = config['CFG']['engine_path']
-		iwad_tf.value = config['CFG']['iwad_path']
-		pwads_tf.value = config['CFG']['pwads']
-		
-		# Verificacion de habilitacion comp
-		pwads_tf.disabled = not eval(config['CFG']['mods_enable'])
-		pwads_bt.disabled = not eval(config['CFG']['mods_enable'])
-		pwads_cb.value = eval(config['CFG']['mods_enable'])
-		#pwads_tf.update(); pwads_bt.update(); pwads_cb.update()
-
-# Limpia los campos de texto
-def clean(engine, iwad, pwads):
-	engine.value, iwad.value, pwads.value = "", "", ""
-	engine.update(); iwad.update(); pwads.update()
-	print(f"(!) Campos limpiados!\n")
-
-# Deshabilita componente
-def disable_comp(*args):
-	print(f"(!) Mods: {args[0].disabled}\n")
-	for comp in args:
-		comp.disabled = not comp.disabled
-		comp.update()
+		else: print(f"(!) Ingrese valores validos para los campos de valores xd")
--- modules/ui.py
+++ modules/ui.py
@@ -12,12 +12,11 @@ def main_ui(page: fl.Page):
     page.title = "Dumex"
     page.window_width, page.window_height = 600, 300
     #page.window_resizable = False # No funciona bien en linux
-    page.update()
 
     # Campos de texto
-    engine_tf = fl.TextField(label="Ruta del Engine (GZDOOM)")
-    iwad_tf = fl.TextField(label="Ruta del .IWAD")
-    pwad_tf = fl.TextField(label="Ruta dir. Mods", disabled=True)
+    engine_tf = fl.TextField(label="Ruta del Engine (GZDOOM)", value=ctrl.set_engine_cfg())
+    iwad_tf = fl.TextField(label="Ruta del .IWAD", value=ctrl.set_iwad_cfg())
+    pwad_tf = fl.TextField(label="Ruta dir. Mods", value=ctrl.set_pwads_cfg(), disabled=ctrl.set_tf_pwads_cfg())
 
     # FileDialog para Engine
     picker_fd_engine = fl.FilePicker(on_result=lambda e: ctrl.get_path(e, val_tf=engine_tf, directory=False)); page.overlay.append(picker_fd_engine)
@@ -29,21 +28,16 @@ def main_ui(page: fl.Page):
 
     # FileDialog para PWADs
     picker_fd_pwad = fl.FilePicker(on_result=lambda e: ctrl.get_path(e, val_tf=pwad_tf, directory=True)); page.overlay.append(picker_fd_pwad)
-    bt_picker_fd_pwad = fl.ElevatedButton(text="Buscar dir. Mods", on_click=lambda _: picker_fd_pwad.get_directory_path(), disabled=True)
+    bt_picker_fd_pwad = fl.ElevatedButton(text="Buscar dir. Mods", on_click=lambda _: picker_fd_pwad.get_directory_path(), disabled=ctrl.set_bt_pwads_cfg())
 
     # CheckBox para habilitar Mods
-    pwad_check_box = fl.Checkbox(label="", value=False, on_change=lambda _: ctrl.disable_comp(pwad_tf, bt_picker_fd_pwad))
+    pwad_check_box = fl.Checkbox(label="", value=ctrl.set_cb_pwads_cfg(), on_change=lambda _: ctrl.disable_comp(pwad_tf, bt_picker_fd_pwad))
 
     # Boton Iniciar xd
     bt_start = fl.ElevatedButton(text="INICIAR", on_click=lambda _: ctrl.exe_all(engine=engine_tf, iwad=iwad_tf, pwads=pwad_tf, pwads_cb=pwad_check_box))
 
     # Boton Limpiar todo xd
-    bt_clean = fl.ElevatedButton(text="Limpiar", on_click=lambda _: ctrl.clean(engine=engine_tf, iwad=iwad_tf, pwads=pwad_tf))
-
-
-    # Verifica configuración
-    ctrl.verify_config(eng_tf=engine_tf, iwad_tf=iwad_tf, pwads_tf=pwad_tf, pwads_bt=bt_picker_fd_pwad, pwads_cb=pwad_check_box)
-    page.update()
+    bt_clean = fl.ElevatedButton(text="Limpiar", on_click=lambda _: ctrl.clean(engine_tf, iwad_tf, pwad_tf))
 
 
     # Añadiendo los widgets a la ventana
@@ -70,3 +64,6 @@ def main_ui(page: fl.Page):
             ])
         ])
     )
+
+    # Actualización de ventana.
+    page.update()
